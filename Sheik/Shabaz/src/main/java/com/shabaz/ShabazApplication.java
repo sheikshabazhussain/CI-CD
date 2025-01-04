7323adb12bd7caaab6b2c215fa5df8a0041bcfcb
sheik-shabaz <sheikh.h@paycorp.io>
2025-01-04 18:10:40 +0530

    pushing files

--- Sheik/Shabaz/src/main/java/com/shabaz/ShabazApplication.java
+++ Sheik/Shabaz/src/main/java/com/shabaz/ShabazApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ImportAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 public class ShabazApplication {
 
 	public static void main(String[] args) {
